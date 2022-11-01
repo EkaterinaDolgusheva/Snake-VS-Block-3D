d7ea72d7e3107f51d7472840b57924ff97febf12
EkaterinaDolgusheva <ekaterina.1694@mail.ru>
2022-11-01 14:37:54 +0500

    Add BlocksManager

---
+++ Assets/Scripts/BlocksManager.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class BlocksManager : MonoBehaviour
+{
+    [Header("Snake manager")]
+
+    public SnakeMovement SM;
+    public float distanceSnakeBarrier;
+
+    [Header("BlockPrefab")]
+
+    public GameObject BlockPrefab;
+
+    [Header("Time To SpawnDelegate management")]
+
+    public float minSpawnTime;
+    public float maxSpawnTime;
+    private float thisTime;
+    private float randomTime;
+
+    [Header("Snake Value for Spawning")]
+
+    public int minSpawnDist;
+    Vector3 previousSnakePos; // заменила вектор 2 на вектор3
+    public List<Vector3> SimpleBoxPositions = new Boo.Lang.List<Vector3>();
+
+
+    private void Start()
+    {
+        thisTime = 0;
+
+        SpawnBarrier();
+
+        randomTime = Random.Range(minSpawnTime, maxSpawnTime);
+    }
+
+    private void Update()
+    {
+        if (GameController.gameState == GameController.GameState.GAME)
+        {
+            if (thisTime < randomTime)
+            {
+                thisTime += Time.deltaTime;
+            }
+            else
+            {
+                SpawnBlocks();
+                thisTime = 0;
+                randomTime = Random.Range (minSpawnTime, maxSpawnTime);
+            }
+
+            if (SM.transform.childCount > 0)
+            {
+                if (SM.transform.GetChild(0).position.y - previousSnakePos.y > minSpawnDist)
+                {
+                    SpawnBarrier();
+                }
+            }
+        }
+    }
+
+    public void SpawnBarrier()
+    {
+        float screenWidthWorldPos = Camera.main.orthographicSize * Screen.width / Screen.height;
+        float distBetweenBlocks = screenWidthWorldPos / 5;
+
+        for (int i = -2; i < 3; i++)
+        {
+            float x = 2 * i * distBetweenBlocks;
+            float y = 0;
+
+            if (SM.transform.childCount > 0)
+            {
+                y = (int)SM.transform.GetChild(0).position.y + distBetweenBlocks * 2 + distanceSnakeBarrier;
+                if (Screen.height / Screen.width == 4 / 3)
+                {
+                    y *= 4 / 3f;
+                }
+            }
+
+            Vector3 spawnPos = new Vector3 (x, y, 0);
+            GameObject boxInstance = Instantiate (BlockPrefab, spawnPos, Quaternion.identity, transform);
+
+            if (SM.transform.childCount > 0)
+            {
+                previousSnakePos = SM.transform.GetChild(0).position;
+            }
+        }
+    }
+
+    public void SpawnBlocks ()
+    {
+        float screenWidthWorldPos = Camera.main.orthographicSize * Screen.width / Screen.height;
+        float distBetweenBlocks = screenWidthWorldPos / 5;
+
+        int random;
+        random = Random.Range(-2, 3);
+
+        float x = 2 * random * distBetweenBlocks;
+        float y = 0; 
+        //наверное надодобавить Z
+
+        if (SM.transform.childCount > 0)
+        {
+            y = (int)SM.transform.GetChild(0).position.y + distBetweenBlocks * 2 + distanceSnakeBarrier;
+            if (Screen.height / Screen.width == 4 / 3)
+            {
+                y *= 2;
+            }
+        }
+
+        Vector3 SpawnPos = new Vector3 (x, y, 0);
+        bool canSpawnBlock = true;
+
+        if (SimpleBoxPositions.Count == 0)
+        {
+            SimpleBoxPositions.Add (SpawnPos);
+        }
+        else
+        {
+            for (int k = 0; k < SimpleBoxPositions.Count; k++)
+            {
+                if (SpawnPos == SimpleBoxPositions[k])
+                {
+                    canSpawnBlock = false;
+                }
+            }
+        }
+
+        GameObject boxInstance;
+
+        if (canSpawnBlock)
+        {
+            SimpleBoxPositions.Add(SpawnPos);
+            boxInstance = Instantiate(BlockPrefab, SpawnPos, Quaternion.identity, transform);
+            boxInstance.name = "SimpleBox";
+            boxInstance.tag = "SimpleBox";
+            boxInstance.layer = LayerMask.NameToLayer("Default");
+            boxInstance.AddComponent<Rigidbody>(); //замена ригидбоди2д на ригидбоди
+            boxInstance.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll; //замена ригидбоди2д на ригидбоди и ригидбоди.констраинтс2д на без 2д
+        }
+    }
+
+    public void SetPreviousSnakePosAfterGameover()
+    {
+        Invoke("PreviousPosInvoke", 0.5f);
+    }
+
+    private void PreviousPosInvoke ()
+    {
+        previousSnakePos.y = SM.transform.GetChild(0).position.y;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public enum GameState {MENU, GAME, GAMEOVER}
+    public static GameState gameState;
+
+    [Header("Managers")]
+
+    public SnakeMovement SM;
+    public BlocksManager BM;
+
+    [Header("CanvasGroup")]
+
+    public CanvasGroup MENU_CG;
+    public CanvasGroup GAME_CG;
+    public CanvasGroup GAMEOVER_CG;
+
+    [Header("ScoreManagement")]
+
+    public Text ScoreText;
+    public Text MenuScoreText;
+    public Text BestScoreText;
+    public static int SCORE;
+    public static int BESTSCORE;
+
+    [Header("SomeBool")]
+
+    bool speedAdded;
+
+
+    private void Start()
+    {
+        SetMenu();
+        SCORE = 0;
+
+        speedAdded = false;
+        BESTSCORE = PlayerPrefs.GetInt("BESTSCORE");
+    }
+
+    private void Update()
+    {
+        ScoreText.text = SCORE + "";
+        MenuScoreText.text = SCORE + "";
+
+        if (SCORE > BESTSCORE)
+        {
+            BESTSCORE = SCORE;
+            BestScoreText.text = BESTSCORE + "";
+
+            if (!speedAdded && SCORE > 150)
+            {
+                SM.speed++;
+                speedAdded = true;
+            }
+        }
+    }
+
+    public void SetMenu()
+    {
+        gameState = GameState.MENU;
+
+        EnableCG(MENU_CG);
+        DisableCG(GAME_CG);
+        DisableCG(GAMEOVER_CG);
+    }
+
+    public void SetGameover ()
+    {
+        gameState = GameState.GAMEOVER;
+
+        EnableCG(MENU_CG);
+        DisableCG(GAME_CG);
+        DisableCG(GAMEOVER_CG);
+
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag ("Box"))
+        {
+            Destroy (g);
+        }
+
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag ("Bar"))
+        {
+            Destroy (g);
+        }
+
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag ("SimpleBox"))
+        {
+            Destroy(g);
+        }
+
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag ("Snake"))
+        {
+            Destroy(g);
+        }
+
+        SM.SpawnBodyPart();
+        BM.SetPreviousSnakePosAfterGameover(); // исправила, добавила как в ВМ СэтПревиоусСнэйкПосАфтерГеймовер
+        speedAdded = false;
+        SM.speed = 3;
+
+        PlayerPrefs.SetInt("Bestscore ", BESTSCORE);
+        BM.SimpleBoxPositions.Clear(); // исправила, добавила с на конце позитион
+    }
+
+    public void EnableCG (CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.blocksRaycasts = true;
+        cg.interactable = true;
+    }
+
+    public void DisableCG (CanvasGroup cg)
+    {
+        cg.alpha = 0;
+        cg.blocksRaycasts = false;
+        cg.interactable = false;
+    }
+
+}
