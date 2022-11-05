fa1864ff35e13968fac7237b4a54ff51bf6c736f
EkaterinaDolgusheva <ekaterina.1694@mail.ru>
2022-11-05 22:02:58 +0500

    Add AutoDestroy

---
+++ Assets/Scripts/FoodBehavior.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBehavior : MonoBehaviour
+{
+    [Header("Snake Manager")]
+
+    SnakeMovement SM;
+    public int foodAmount;
+
+    private void Start()
+    {
+        SM = GameObject.FindGameObjectWithTag("SnakeManager").GetComponent <SnakeMovement>();
+        foodAmount = Random.Range(1, 10);
+        transform.GetComponentInChildren<TextMesh>().text = "" + foodAmount;
+    }
+
+    private void Update()
+    {
+        if (SM.transform.childCount > 0 && transform.position.y - SM.transform.GetChild(0).position.y < -10)
+        {
+            Destroy (this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
